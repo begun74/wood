57b3323507f6135f69c04d21d14726cb87a1af05
begun <begun75@gmail.com>
2016-08-11 15:44:51 +0200

    656

---
+++ src/main/java/wood/controller/WControllerError.java
@@ -0,0 +1,26 @@
+package wood.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class WControllerError {
+
+    @RequestMapping(value = "/403" , method = RequestMethod.GET)
+	public String m403() {
+	 
+	    return "403";
+	}
+
+    @RequestMapping(value = "/404" , method = RequestMethod.GET)
+	public String m404() {
+	 
+	    return "404";
+	}
+
+    @RequestMapping(value = "/500" , method = RequestMethod.GET)
+	public String m500() {
+	 
+	    return "500";
+	}
+
+}
